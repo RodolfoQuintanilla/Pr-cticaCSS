ca1c3f265bbc976a4d888b5c12fe3be636c40c1a
Rodolfo <rqm@outlook.es>
2022-03-07 18:43:56 -0600

    icinos amarillos

--- vacuna/src/components/Imagenes.jsx
+++ vacuna/src/components/Imagenes.jsx
@@ -1,10 +1,30 @@
 import React from 'react';
 import vacuna from '../img/vacuna.jpg'
+import styles from '../style/Img.module.css'
 
 const Imagenes = () => {
     return (
         <section>
-            <img src={vacuna} alt="vacuna" />
+            <div className={styles.Texto}>
+                <h1>Lucha Contra <br />el Virus con Vacunas</h1>
+                <p>Paremos esta pandemia matando al virus
+                    con una vacuna, no permita que usted y su
+                    la familia se infecta</p>
+                <div className={styles.textitos}>
+                    <p> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-shield-fill-check" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd" d="M8 0c-.69 0-1.843.265-2.928.56-1.11.3-2.229.655-2.887.87a1.54 1.54 0 0 0-1.044 1.262c-.596 4.477.787 7.795 2.465 9.99a11.777 11.777 0 0 0 2.517 2.453c.386.273.744.482 1.048.625.28.132.581.24.829.24s.548-.108.829-.24a7.159 7.159 0 0 0 1.048-.625 11.775 11.775 0 0 0 2.517-2.453c1.678-2.195 3.061-5.513 2.465-9.99a1.541 1.541 0 0 0-1.044-1.263 62.467 62.467 0 0 0-2.887-.87C9.843.266 8.69 0 8 0zm2.146 5.146a.5.5 0 0 1 .708.708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7.5 7.793l2.646-2.647z" />
+                    </svg> Mejor Protección</p>
+                    <p><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16">
+                        <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
+                    </svg>
+                        Vacunas Seleccionadas</p>
+                </div>
+
+            </div>
+            <div>
+                <img src={vacuna} alt="vacuna" className={styles.ImagenVacuna} />
+            </div>
+
         </section>
     );
 };
