cfcbd62c117c68af003637628aab8881ab4796a4
Rodolfo <rqm@outlook.es>
2022-03-10 20:19:56 -0600

    Final

--- vacuna/src/components/Footer.jsx
+++ vacuna/src/components/Footer.jsx
@@ -3,7 +3,41 @@ import style from '../style/Footer.module.css'
 const Footer = () => {
     return (
         <footer className={style.fondo}>
-            footer
+            <div className={style.uno}>
+                <h1>Vacunación</h1>
+                <p>Nuestro objetivo es ayudar a los <br />
+                    mundo libre de la<br />
+                    pandemia en curso</p>
+            </div>
+
+            <div className={style.dos}>
+                <div>
+                    <ul>
+                        <li> <h2>About</h2> </li>
+                        <li>About</li>
+                        <li>Features</li>
+                        <li>News </li>
+                    </ul>
+                </div>
+                <div>
+                    <ul>
+                        <li> <h2>About</h2> </li>
+                        <li>About</li>
+                        <li>Features</li>
+                        <li>News </li>
+                    </ul>
+                </div>
+                <div>
+                    <ul>
+                        <li> <h2>About</h2> </li>
+                        <li>About</li>
+                        <li>Features</li>
+                        <li>News </li>
+                    </ul>
+                </div>
+
+            </div>
+
         </footer>
     );
 };
